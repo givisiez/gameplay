6d2dd7b51f7d7b8242c802af07ab7c3ec9e36560
givisiez <8727915+givisiez@users.noreply.github.com>
2021-11-18 09:52:30 -0300

    update

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,36 +1,56 @@
 import React, { useState } from "react";
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 
-import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
+import { theme } from "../../global/styles/theme";
+import Icon from "@expo/vector-icons/Feather";
+import { GuildIcon } from "../../components/GuildIcon";
 
 import { Background } from "../../components/Background";
 import { CategorySelect } from "../../components/CategorySelect";
 import { Header } from "../../components/Header";
 
-export function AppointmentCreate(){
-  const [category, setCategory] = useState('');
-
+export function AppointmentCreate() {
+  const [category, setCategory] = useState("");
   return (
     <Background>
-      <Header 
-        title="Agendar partida"       
-      />
+      <Header title="Agendar partida" />
 
-      <Text style={styles.label}>
+      <Text
+        style={[
+          styles.label,
+          {
+            marginLeft: 24,
+            marginTop: 36,
+            marginBottom: 18,
+          },
+        ]}
+      >
         Categoria
       </Text>
 
-      <CategorySelect 
+      <CategorySelect
         hasCheckBox
         setCategory={setCategory}
         categorySelected={category}
       />
 
       <View style={styles.form}>
-        
-      </View>
+        <RectButton>
+          <View style={styles.select}>
+            {
+              //<View style={styles.image}/>
+              <GuildIcon />
+            }
+            <View style={styles.selectBody}>
+              <Text style={styles.label}>Selecione um servidor</Text>
+            </View>
 
+            <Icon name="chevron-right" color={theme.colors.heading} size={18} />
+          </View>
+        </RectButton>
+      </View>
     </Background>
   );
 }
