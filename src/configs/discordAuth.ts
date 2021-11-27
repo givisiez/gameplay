0ec5abc0511d440a797d2b20db03c38b17366e0f
givisiez <8727915+givisiez@users.noreply.github.com>
2021-11-27 10:07:42 -0300

    autenticacao discord

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,15 @@
+const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-36618d23-0eb2-477b-8637-cfda8b2886e5';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '912817626114322453';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+
+
+export {
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE
+}
