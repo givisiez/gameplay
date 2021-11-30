084045d2573c773f0dac97c3244152670ab75e48
givisiez <8727915+givisiez@users.noreply.github.com>
2021-11-29 21:01:23 -0300

    autenticação/context

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useAuth } from '../../hooks/auth';
 
 import { Avatar } from '../Avatar';
 import { styles } from './styles';
 
 export function Profile() {
+  const { user } = useAuth();
+  console.log(user);
   return(
     <View style={styles.container}>
 
-      <Avatar urlImage="https://github.com/givisiez.png"/>
+      <Avatar urlImage={ user.avatar }/>
 
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>
             Olá!
           </Text>
-
           <Text style={styles.userName}>
-            Leandro
+            { user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1) }
           </Text>
         </View>
         
